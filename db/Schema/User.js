d61defd68e98f428f0427de2b69081d316bac34b
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2021-05-03 16:57:28 +0300

    pet-charity-be-feature/animal-profile - fixed problems 03/05

--- db/Schema/User.js
+++ db/Schema/User.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("user", UserSchema);
