a634ea45c2ba921dafddb1b0c1168724a1cc28fa
alexander-svirzhevsky <alex-ander.1@mail.ru>
2021-06-14 18:09:18 +0300

    pet-charity-be-feature/manual-testing handled some errors when user tries to register

--- services/user.js
+++ services/user.js
@@ -4,26 +4,26 @@ const User = require("../db/Schema/User");
 const AlreadyExists = require("../utils/errors/AlreadyExists");
 
 async function register({ name, email, password, isAdmin = false }) {
-	const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-	if (user) {
-		throw new AlreadyExists("User already exists");
-	}
+  if (user) {
+    throw new AlreadyExists("User with this email already exists");
+  }
 
-	const hashedPassword = await argon2.hash(password);
+  const hashedPassword = await argon2.hash(password);
 
-	const newUser = new User({
-		name,
-		email,
-		password: hashedPassword,
-		isAdmin,
-	});
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    isAdmin,
+  });
 
-	await newUser.save();
+  await newUser.save();
 
-	return newUser;
+  return newUser;
 }
 
 module.exports = {
-	register,
+  register,
 };
