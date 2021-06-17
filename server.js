3d7a9be36a6df18ad383271b18e39567f31aa59a
Artsiom Kashyn <umalishenni@gmail.com>
2021-06-17 13:16:49 +0200

    added production configuration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const path = require("path");
 const express = require("express");
 const app = express();
 
@@ -9,8 +10,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {});
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/googleAuth", require("./routes/api/googleAuth"));
 app.use("/api/auth", require("./routes/api/auth"));
@@ -19,10 +18,20 @@ app.use("/", require("./routes/api/animal"));
 app.use("/api/breed", require("./routes/api/breed"));
 app.use("/api/profile", require("./routes/api/profile"));
 
+if (process.env.NODE_ENV === "production") {
+	const pathToClientFolder = path.join(__dirname + "/client/build");
+
+	app.use(express.static(pathToClientFolder));
+
+	app.get("*", (_, res) => {
+		res.sendFile(path.join(pathToClientFolder + "/index.html"));
+	});
+}
+
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server starting on port ${PORT}`);
+	console.log(`Server starting on port ${PORT}`);
 });
