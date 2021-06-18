4d3cc6e2d7e07c75962495a40a168415b3844f22
alexander-svirzhevsky <alex-ander.1@mail.ru>
2021-06-18 14:45:52 +0300

    pet-charity-be-feature/swagger added swagger to some routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,17 +4,69 @@ const { check } = require("express-validator");
 
 const userController = require("../../controllers/user");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Registration
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     name: Registration
+ *     summary: Register  a user
+ *     tags: [Registration]
+ *     consumes:
+ *       - application/json
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *           example:
+ *                {
+ *                   "email":"alexander@gmail.com",
+ *                   "password":"123123123",
+ *                   "confirmPassword":"123123123",
+ *                   "name":"alexander",
+ *                }
+ *         required:
+ *           - username
+ *           - password
+ *     responses:
+ *        '200':
+ *         description: Return token
+ *         content:
+ *           application/json; charset=utf-8:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                token:
+ *                  type: string
+ *             example: {"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJ1bml2ZXJzZXp4Y3ZAZ21haWwuY29tIiwiaWF0IjoxNTY4MDIzMjAyLCJleHAiOjE1NjgwMjMyMzh9._apV-RyrCHS0miAE0S9pt-06t6x3Xty-skuIWuLGp_k"}
+ */
+
 router.post(
-	"/",
-	[
-		check("name", "Name is required").not().isEmpty(),
-		check("email", "Please include a valid email").isEmail(),
-		check(
-			"password",
-			"Please enter a password with 6 or more characters"
-		).isLength({ min: 6 }),
-	],
-	userController.register
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  userController.register
 );
 
 module.exports = router;
